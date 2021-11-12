61fb0b455dc31d41b3f186f5f073aee7cf87b394
ArturoAmaya <47407486+ArturoAmaya@users.noreply.github.com>
2021-11-12 14:26:36 -0800

    Update badlyformatted.js

--- badlyformatted.js
+++ badlyformatted.js
@@ -1,3 +1,3 @@
-function myFunc (mynewVar, myOtherVar, myDoubleVar, OMGitsaLet, THISISALOTOFVARS, wowantoherone, thisisgettingdumb ) {
+function myFunc (mynewVar, myOtherVar, myDoubleVar, OMGitsaLet, THISISALOTOFVARS, wowantoherone, thisisgettingdumb, wow ) {
   console.log("Hello World!");
 }
