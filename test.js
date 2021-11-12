24cb00a39a887ee4d15b32489c88e15ffe4ffce1
TuckerFrandsen <74218751+TuckerFrandsen@users.noreply.github.com>
2021-11-12 11:51:58 -0800

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 console.log("Hello world");
 // This is just a sample script. Paste your real code (javascript or HTML) here.
-
+var b, c, d, e, f;
 if ('this_is'==/an_example/){of_beautifier();}else{var a=b?(c%d):e[f];}
