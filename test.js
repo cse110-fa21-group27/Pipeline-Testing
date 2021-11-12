dab8da795fba57740506671e6db19193dfe3589c
TuckerFrandsen <TuckerFrandsen@users.noreply.github.com>
2021-11-12 22:52:27 +0000

    Prettified Code!

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
 // This is just a sample script. Paste your real code (javascript or HTML) here.
 
-if ('this_is'==/an_example/){of_beautifier();}else{var a=b?(c%d):e[f];}
+if ('this_is' == /an_example/) {
+    of_beautifier()
+} else {
+    var a = b ? c % d : e[f]
+}
