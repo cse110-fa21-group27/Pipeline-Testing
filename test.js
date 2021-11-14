ee4b71f213b3811444136038ca288081f929e900
TuckerFrandsen <74218751+TuckerFrandsen@users.noreply.github.com>
2021-11-14 13:06:29 -0800

    Checking prettify commit message

--- test.js
+++ test.js
@@ -3,8 +3,6 @@ console.log('Hello world')
 var b, c, d, e, f
 // This is just a sample script. Paste your real code (javascript or HTML) here.
 
-if ('this_is' == /an_example/) {
-  of_beautifier()
-} else {
-  var a = b ? c % d : e[f]
-}
+// This is just a sample script. Paste your real code (javascript or HTML) here.
+
+if ('this_is'==/an_example/){of_beautifier();}else{var a=b?(c%d):e[f];}
