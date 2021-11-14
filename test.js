8270fa99bac66ba6e354214ba3e38d63a6843f6c
TuckerFrandsen <74218751+TuckerFrandsen@users.noreply.github.com>
2021-11-14 14:44:32 -0800

    Update test.js

--- test.js
+++ test.js
@@ -3,10 +3,4 @@ console.log('Hello world')
 var b, c, d, e, f
 // This is just a sample script. Paste your real code (javascript or HTML) here.
 
-// This is just a sample script. Paste your real code (javascript or HTML) here.
-
-if ('this_is' == /an_example/) {
-  of_beautifier()
-} else {
-  var a = b ? c % d : e[f]
-}
+if ('this_is'==/an_example/){of_beautifier();}else{var a=b?(c%d):e[f];}
