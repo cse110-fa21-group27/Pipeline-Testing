99f7b76870a276b1537189e7c02502cbcc47d420
TuckerFrandsen <74218751+TuckerFrandsen@users.noreply.github.com>
2021-11-14 13:22:30 -0800

    Update test.js

---
+++ test.js
@@ -0,0 +1,12 @@
+console.log('Hello world')
+// This is just a sample script. Paste your real code (javascript or HTML) here.
+var b, c, d, e, f
+// This is just a sample script. Paste your real code (javascript or HTML) here.
+
+// This is just a sample script. Paste your real code (javascript or HTML) here.
+
+if ('this_is' == /an_example/) {
+  of_beautifier()
+} else {
+  var a = b ? c % d : e[f]
+}
